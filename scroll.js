0e7bc6d15c529d5c04be73bed1ea27f0528c5908
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-10-08 17:29:01 +0200

    text section 1

--- scroll.js
+++ scroll.js
@@ -1,7 +1,18 @@
 const scrollContainer = document.getElementById('scrollContainer');
 const scrollMaxY = scrollContainer.scrollHeight;
+const numberOfSections = 6;
+
 //divide total scrolling into 6 section
+// console.log the current section
+
 const navProgress = () => {
-	console.log('top: '  + (scrollContainer.pageYOffset || scrollContainer.scrollTop));
-	console.log(scrollContainer.scrollHeight);
+	// console.log('top: '  + (scrollContainer.pageYOffset || scrollContainer.scrollTop));
+	// console.log(scrollContainer.scrollHeight);
+	let thresholds = createSectionsThreshold(numberOfSections, scrollMaxY );
+	console.log(scrollContainer.scrollTop % (scrollMaxY/numberOfSections));
+
+}
+
+const createSectionsThreshold = (numOfSections, maxScroll) => {
+
 }
