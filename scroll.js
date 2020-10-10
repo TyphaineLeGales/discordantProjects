5dcfde7c76ee40382dc5b40d7fc10c247c7ee4f7
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-10-10 12:41:07 +0200

    images grid done

--- scroll.js
+++ scroll.js
@@ -1,6 +1,8 @@
 const scrollContainer = document.getElementById('scrollContainer');
 const scrollMaxY = scrollContainer.scrollHeight;
 const numberOfSections = 6;
+const sections = document.querySelectorAll('div.section');
+let currSection;
 
 //divide total scrolling into 6 section
 // console.log the current section
@@ -8,11 +10,23 @@ const numberOfSections = 6;
 const navProgress = () => {
 	// console.log('top: '  + (scrollContainer.pageYOffset || scrollContainer.scrollTop));
 	// console.log(scrollContainer.scrollHeight);
-	let thresholds = createSectionsThreshold(numberOfSections, scrollMaxY );
-	console.log(scrollContainer.scrollTop % (scrollMaxY/numberOfSections));
+	// let thresholds = createSectionsThreshold(numberOfSections, scrollMaxY );
+	// console.log(scrollContainer.scrollTop % (scrollMaxY/numberOfSections));
+	if(scrollContainer.scrollTop > 300) {
+		dispCurrSection(getCurrSection(1));
+	}
 
 }
 
-const createSectionsThreshold = (numOfSections, maxScroll) => {
+// const createSectionsThreshold = (numOfSections, maxScroll) => {
 
+// }
+
+const getCurrSection = (index) => {
+	return sections[index];
+}
+
+const dispCurrSection = (section) => {
+	sections.forEach(section => section.classList.add('invisible')); //exclude current section and add 'invisible'
+	section.classList.remove('invisible');
 }
