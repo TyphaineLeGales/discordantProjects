689d2ae763a3969157628888885349693b3d7fd0
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-10-13 17:51:00 +0200

    piling images + switching captions and description working when user scroll down

--- scroll.js
+++ scroll.js
@@ -1,6 +1,7 @@
 const scrollContainer = document.getElementById('scrollContainer');
 const sections = document.querySelectorAll('div.section');
 const descriptions = document.querySelectorAll('div.project_description');
+const captions = document.querySelectorAll('p.caption');
 const numberOfSections = sections.length;
 let prevSectionIndex = 0;
 let currSectionIndex = 0;
@@ -13,9 +14,8 @@ const navSections = () => {
 	let mappedScroll = mapRange(scrollContainer.scrollTop,0, scrollContainer.scrollHeight, 0, numberOfSections);
 	currSectionIndex = Math.trunc(mappedScroll);
 	if(currSectionIndex != prevSectionIndex) {
-		dispCurrDescription();
-		// dispCurrBottomItem();
-		dispCurrSection(sections[currSectionIndex]);
+
+		dispCurrDescription(sections[currSectionIndex]);
 	}
 	navImagesWithinSection(sections[currSectionIndex], mappedScroll);
 	prevSectionIndex = currSectionIndex;
@@ -24,47 +24,31 @@ const navSections = () => {
 
 const navImagesWithinSection = (section, scroll) => {
 	let images = section.querySelectorAll('img');
-	let captions = section.querySelectorAll('p.caption');
+	let captionsWithinSection = section.querySelectorAll('p.caption');
 
 	currImageIndex = Math.trunc(mapRange(scroll, currSectionIndex,  currSectionIndex+1, 0, images.length));
 
-	if(currImageIndex > prevImageIndex) {
+	if(currImageIndex >= prevImageIndex) {
 		addElement(images[currImageIndex]);
 	}  else if (currImageIndex < prevImageIndex && prevSectionIndex >= currSectionIndex) {
 		removeElement(images[prevImageIndex]);
 	}
 
-	dispImgCaptions(captions, currImageIndex);
+	dispImgCaptions(captionsWithinSection, currImageIndex);
 	prevImageIndex = currImageIndex;
 } 
 
-const dispCurrDescription = () => {
-	descriptions[prevSectionIndex].classList.add('invisible');
-	descriptions[currSectionIndex].classList.remove('invisible');
-	console.log(descriptions[currSectionIndex]);
-}
 
-// const dispCurrBottomItem = () => {
-// 	bottomItems[prevSectionIndex].classList.add('invisible');
-// 	bottomItems[currSectionIndex].classList.add('invisible');
-// }
-
-const removePrevBottomItem = (section) => {
-	let bottomItem = document.querySelector('div.bottomItem');
-	bottomItem.classList.add('invisible');
-}
-
-const dispCurrSection = (section) => {
+const dispCurrDescription = (section) => {
 	let description = section.querySelector('div.project_description');
 	descriptions.forEach(section => section.classList.add('invisible')); //exclude current section and add 'invisible'
 	description.classList.remove('invisible');
-	section.classList.remove('invisible');
 }
 
-const dispImgCaptions = (captions, index) => {
-	if(captions[index]) {
-		captions.forEach(caption => caption.classList.add('invisible'));
-		captions[currImageIndex].classList.remove('invisible');
+const dispImgCaptions = (captionsWithinSection, index) => {
+	if(captionsWithinSection[index]) {
+		captions.forEach(caption => caption.classList.add('notDisplayed'));
+		captionsWithinSection[currImageIndex].classList.remove('notDisplayed');
 	}
 }
 
