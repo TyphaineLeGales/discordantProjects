15bb9d186c88c6f08963c1441229b46dcd4f7e0d
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-10-13 17:12:56 +0200

    piling up images and seperate description animation works

--- scroll.js
+++ scroll.js
@@ -1,14 +1,20 @@
 const scrollContainer = document.getElementById('scrollContainer');
 const sections = document.querySelectorAll('div.section');
+const descriptions = document.querySelectorAll('div.project_description');
 const numberOfSections = sections.length;
 let prevSectionIndex = 0;
 let currSectionIndex = 0;
 let prevImageIndex = 0, currImageIndex=0;
 
+// add section on top of each other
+// project description goes invisible
+
 const navSections = () => {
 	let mappedScroll = mapRange(scrollContainer.scrollTop,0, scrollContainer.scrollHeight, 0, numberOfSections);
 	currSectionIndex = Math.trunc(mappedScroll);
 	if(currSectionIndex != prevSectionIndex) {
+		dispCurrDescription();
+		// dispCurrBottomItem();
 		dispCurrSection(sections[currSectionIndex]);
 	}
 	navImagesWithinSection(sections[currSectionIndex], mappedScroll);
@@ -22,7 +28,6 @@ const navImagesWithinSection = (section, scroll) => {
 
 	currImageIndex = Math.trunc(mapRange(scroll, currSectionIndex,  currSectionIndex+1, 0, images.length));
 
-
 	if(currImageIndex > prevImageIndex) {
 		addElement(images[currImageIndex]);
 	}  else if (currImageIndex < prevImageIndex && prevSectionIndex >= currSectionIndex) {
@@ -33,8 +38,26 @@ const navImagesWithinSection = (section, scroll) => {
 	prevImageIndex = currImageIndex;
 } 
 
+const dispCurrDescription = () => {
+	descriptions[prevSectionIndex].classList.add('invisible');
+	descriptions[currSectionIndex].classList.remove('invisible');
+	console.log(descriptions[currSectionIndex]);
+}
+
+// const dispCurrBottomItem = () => {
+// 	bottomItems[prevSectionIndex].classList.add('invisible');
+// 	bottomItems[currSectionIndex].classList.add('invisible');
+// }
+
+const removePrevBottomItem = (section) => {
+	let bottomItem = document.querySelector('div.bottomItem');
+	bottomItem.classList.add('invisible');
+}
+
 const dispCurrSection = (section) => {
-	sections.forEach(section => section.classList.add('invisible')); //exclude current section and add 'invisible'
+	let description = section.querySelector('div.project_description');
+	descriptions.forEach(section => section.classList.add('invisible')); //exclude current section and add 'invisible'
+	description.classList.remove('invisible');
 	section.classList.remove('invisible');
 }
 
